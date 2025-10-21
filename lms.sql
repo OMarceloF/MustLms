e7d4150bf1b690a68c9d74b144995aad23724cc7
ArthurSaraiva25 <arthurlsaraiva24@gmail.com>
2025-10-21 11:24:29 -0300

    SQL

--- lms.sql
+++ lms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 21/10/2025 às 16:16
+-- Tempo de geração: 21/10/2025 às 16:23
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
